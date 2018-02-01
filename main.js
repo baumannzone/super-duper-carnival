f6736a1b6edfa5e04d99347ff9c3bfc2a237b3ee
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2018-02-01 21:16:59 +0100

    styles

--- main.js
+++ main.js
@@ -1,44 +1,48 @@
 'use strict';
 
-const candidates = [ 'ancoar', 'codingCarlos', 'ymd', 'jbaumann', 'josheriff' ];
+// const candidates = [ 'ancoar', 'codingCarlos', 'ymd', 'jbaumann', 'josheriff' ];
+const candidates = [ 'jbaumann', 'josheriff' ];
 let liometer = 0;
+
+// Create style classes
+let styles = document.getElementById( 'styles' );
+let styleText = '<style>';
+candidates.forEach( function ( candidate ) {
+  styleText += `.${candidate} {
+  background: url("img/${candidate}.png");
+  background-size: cover;
+  background-position: center;
+}`;
+} );
+styleText += '</style>';
+styles.innerHTML = styleText;
 
 function initMess() {
   const cells = document.querySelectorAll( '.item' );
   const randomCellNum = Math.floor( Math.random() * 20 );
   const randomPersonNum = Math.floor( Math.floor( Math.random() * candidates.length ) );
 
-  console.log( 'Cells: ', Array.from( cells ) );
-  console.log( 'randomCellNum: ', randomCellNum );
-  console.log( 'randomPersonNum: ', randomPersonNum );
-
   // Select one cell
   const selectedCell = cells[ randomCellNum ];
+
   // Select one person
   const selectedPerson = candidates[ randomPersonNum ];
-  // Image path src
-  const path = `./img/${selectedPerson}.png`;
 
-  // Add image
-  selectedCell.innerHTML = `<img src="${path}" alt="${selectedPerson}">`;
+  // Agregar clase con la img
+  selectedCell.classList.add( selectedPerson );
 
   // On click
-  selectedCell.addEventListener( 'click', function ( ev ) {
-    const id = this.id;
-    upLiometer()
-    console.debug( '+1 liado..', id );
-  } );
-
+  selectedCell.addEventListener( 'click', upLiometer );
 
   // Repeat after xxx ms
   setTimeout( function () {
-    clear( selectedCell );
+    clear( selectedCell, selectedPerson );
   }, 1500 );
 }
 
-function clear( selectedCell ) {
-  selectedCell.innerHTML = '';
-  console.log(liometer,'ºººººº liometer <<<<<<<')
+function clear( selectedCell, selectedPerson ) {
+  selectedCell.classList.remove( selectedPerson );
+  selectedCell.removeEventListener( 'click', upLiometer );
   if ( liometer < 5 ) {
     initMess();
   }
@@ -48,10 +52,10 @@ function clear( selectedCell ) {
 }
 
 // On click
-function upLiometer() {
-  console.log( 'Liometro:', liometer );
-  alert( '¡Has sido liado!' );
+function upLiometer( ev ) {
   liometer++;
+  const liometerBar = document.getElementById( 'liometer-bar' );
+  liometerBar.setAttribute( 'value', liometer );
 }
 
-document.addEventListener( "DOMContentLoaded", initMess, false );
+document.addEventListener( 'DOMContentLoaded', initMess, false );
